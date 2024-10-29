64d47faa612b20ea9d94d514af9e825f1233bc24
Steven Lopez <stevenlopez009r@gmail.com>
2024-10-28 19:07:14 -0500

    creacion de resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,13 +1,135 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 const Resume = () => {
   return (
-    <Box
-      id="resume"
-      sx={{ width: "70%", paddingTop: "10vh", minHeight: "100vh" }}
-    >
-      <h2>Resume</h2>
-      <p>Contenido Resume</p>
+    <Box id="resume" sx={{ width: "100%", minHeight: "120vh" }}>
+      <Typography variant="h5" sx={{ color: "white", marginTop: "10vh" }}>
+        My Education
+      </Typography>
+      <TableContainer
+        component={Paper}
+        sx={{ maxWidth: "100%", backgroundColor: "transparent" }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "transparent" }}>
+              <TableCell
+                sx={{ color: "white", fontWeight: "bold", fontSize: "1.1rem" }}
+              >
+                Institution
+              </TableCell>
+              <TableCell
+                sx={{ color: "white", fontWeight: "bold", fontSize: "1.1rem" }}
+              >
+                Degree
+              </TableCell>
+              <TableCell
+                sx={{ color: "white", fontWeight: "bold", fontSize: "1.1rem" }}
+              >
+                Year
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+              sx={{
+                height: "100px",
+                "&:nth-of-type(even)": { backgroundColor: "transparent" },
+              }}
+            >
+              <TableCell sx={{ color: "white" }}>SENA</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Tecnologo en Analisis y Desarrollo de Software
+              </TableCell>
+              <TableCell sx={{ color: "white" }}>2022-2024</TableCell>
+            </TableRow>
+            <TableRow
+              sx={{
+                height: "100px",
+                "&:nth-of-type(even)": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              <TableCell sx={{ color: "white" }}>UDEMY</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Master's in Data Science
+              </TableCell>
+              <TableCell sx={{ color: "white" }}>2023-2024</TableCell>
+            </TableRow>
+            <TableRow
+              sx={{
+                height: "100px",
+                "&:nth-of-type(even)": { backgroundColor: "#e0e0e0" },
+              }}
+            >
+              <TableCell sx={{ color: "white" }}>Institute C</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Diploma in Web Development
+              </TableCell>
+              <TableCell sx={{ color: "white" }}>2020</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Typography variant="h5" sx={{ color: "white", marginTop: "10vh" }}>
+        Work Experience
+      </Typography>
+      <TableContainer
+        component={Paper}
+        sx={{
+          maxWidth: "100%",
+          backgroundColor: "transparent",
+          marginTop: "5vh",
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "transparent" }}>
+              <TableCell
+                sx={{ color: "white", fontWeight: "bold", fontSize: "1.1rem" }}
+              >
+                Empresa
+              </TableCell>
+              <TableCell
+                sx={{ color: "white", fontWeight: "bold", fontSize: "1.1rem" }}
+              >
+                Cargo
+              </TableCell>
+              <TableCell
+                sx={{ color: "white", fontWeight: "bold", fontSize: "1.1rem" }}
+              >
+                Tiempo
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+              sx={{
+                height: "100px",
+                "&:nth-of-type(even)": { backgroundColor: "transparent" },
+              }}
+            >
+              <TableCell sx={{ color: "white" }}>Vertebra Soluciones</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Desarrollador Frontend, Apoyo QA
+              </TableCell>
+              <TableCell sx={{ color: "white" }}>7 meses</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
